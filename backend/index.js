96dd89ea07b70a96bd3d6f120377a5be42172eec
SUNISH <sunish@SUNISHs-MacBook-Air.local>
2025-06-29 00:28:09 +0530

    refactoring URL

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["https://hardware-crm-qaoe.vercel.app/","http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["https://hardware-crm-qaoe.vercel.app","http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
