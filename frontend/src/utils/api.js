7c7a1b8ef632d9a736955d17afc780f556c80ac7
SUNISH <sunish@SUNISHs-MacBook-Air.local>
2025-06-29 00:20:25 +0530

    Added backend URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://hardware-crm.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
